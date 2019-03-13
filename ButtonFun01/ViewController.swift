2fc30c2500ede8b29fcb72423b5a36b776af0487
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-13 15:32:15 +0900

    Push

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySubView: UIView!
+    var checked = false;
     override func viewDidLoad() {
+        
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        self.view.backgroundColor = UIColor.yellow
+        
+        print(myLabel.text!)
+        myLabel.text = "Hello IPhone!"
+        
+        mySubView.backgroundColor = UIColor.red
     }
 
+    
+    @IBAction func ButtoPressed(_ sender: Any) {
+    
+        
+        
+        if(checked==false) {
+            print("버튼을 눌렀습니다. / blue")
+            myLabel.text = "Button Pressed / Blue"
+            mySubView.backgroundColor = UIColor.blue
+            checked = true
+        }
+        else {
+            print("버튼을 눌렀습니다. / red")
+            myLabel.text = "Button Pressed / Red"
+            mySubView.backgroundColor = UIColor.red
+            checked = false
+        }
+        
+        
+    }
+    
+    
+    
+    
 
 }
 
